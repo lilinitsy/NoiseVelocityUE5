899b8353a6b05fda4f3cb3446b6c5a7463239a51
ville <lilinitsy@gmail.com>
2025-11-10 14:10:18 -0600

    Remove forward declaration from test character

--- Source/NoiseVelocity/ExampleShaderTestCharacter.h
+++ Source/NoiseVelocity/ExampleShaderTestCharacter.h
@@ -29,6 +29,6 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	TSharedPtr<class FCustomComputeShadersViewExtension, ESPMode::ThreadSafe> view_extension;
+	TSharedPtr<FCustomComputeShadersViewExtension, ESPMode::ThreadSafe> view_extension;
 	
 };
