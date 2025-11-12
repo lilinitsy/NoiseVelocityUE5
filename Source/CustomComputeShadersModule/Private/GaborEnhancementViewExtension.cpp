bdc7d84dde678415ffe9737649f6577fb14f3faf
ville <lilinitsy@gmail.com>
2025-11-12 17:36:50 -0600

    Get gabor convolutions working, required mips to be made in the view extension

--- Source/CustomComputeShadersModule/Private/GaborEnhancementViewExtension.cpp
+++ Source/CustomComputeShadersModule/Private/GaborEnhancementViewExtension.cpp
@@ -1,8 +1,10 @@
 #include "GaborEnhancementViewExtension.h"
 #include "CustomComputeShaders.h"
 
+#include "GenerateMips.h"
 #include "PostProcess/PostProcessing.h"
 #include "RenderGraphUtils.h"
+
 
 FGaborEnhancementViewExtension::FGaborEnhancementViewExtension(const FAutoRegister& auto_register, FVector2f foveation_center, float radius_fovea, float radius_periphery, float screen_width_cm, float screen_height_cm, float distance_from_screen_cm, float blur_rate_arcmin_per_degree, float s_k, unsigned int cells, unsigned int impulses_per_cell, unsigned int seed) :
 	FSceneViewExtensionBase(auto_register),
@@ -31,48 +33,47 @@ void FGaborEnhancementViewExtension::PrePostProcessPass_RenderThread(
 	FRDGTextureRef scene_color = (*inputs.SceneTextures)->SceneColorTexture;
 	FRDGTextureDesc desc = scene_color->Desc;
 	desc.Flags |= TexCreate_UAV;
+	desc.NumMips = 5;
 
-	// 1) blur output
 	FRDGTextureRef blur_output = graph_builder.CreateTexture(desc, TEXT("gaussian_blur_output"));
 
-	// 2) dispatch gaussian blur
-	{
-		TShaderMapRef<FGaussianBlurCS> blur_cs(GetGlobalShaderMap(GMaxRHIFeatureLevel));
-		FGaussianBlurCS::FParameters* blur_params = graph_builder.AllocParameters<FGaussianBlurCS::FParameters>();
+	TShaderMapRef<FGaussianBlurCS> blur_cs(GetGlobalShaderMap(GMaxRHIFeatureLevel));
+	FGaussianBlurCS::FParameters* blur_params = graph_builder.AllocParameters<FGaussianBlurCS::FParameters>();
 
-		blur_params->input_texture = scene_color;
-		blur_params->Input_Sampler = TStaticSamplerState<SF_Bilinear>::GetRHI();
-		blur_params->output_texture = graph_builder.CreateUAV(blur_output);
+	blur_params->input_texture = scene_color;
+	blur_params->Input_Sampler = TStaticSamplerState<SF_Bilinear>::GetRHI();
+	blur_params->output_texture = graph_builder.CreateUAV(blur_output);
 
-		// set your foveation stuff here...
-		blur_params->foveation_center = foveation_center;
-		blur_params->radius_fovea = radius_fovea;
-		blur_params->radius_periphery = radius_periphery;
-		blur_params->screen_width_cm = screen_width_cm;
-		blur_params->screen_height_cm = screen_height_cm;
-		blur_params->distance_from_screen = distance_from_screen_cm; // for gaussian blur, it's just called "distance_from_screen"
+	blur_params->foveation_center = foveation_center;
+	blur_params->radius_fovea = radius_fovea;
+	blur_params->radius_periphery = radius_periphery;
+	blur_params->screen_width_cm = screen_width_cm;
+	blur_params->screen_height_cm = screen_height_cm;
+	blur_params->distance_from_screen = distance_from_screen_cm; // for gaussian blur, it's just called "distance_from_screen"
 
-		const FIntVector group_count(
-			FMath::DivideAndRoundUp(desc.Extent.X, 16),
-			FMath::DivideAndRoundUp(desc.Extent.Y, 16),
-			1);
+	const FIntVector group_count(
+		FMath::DivideAndRoundUp(desc.Extent.X, 16),
+		FMath::DivideAndRoundUp(desc.Extent.Y, 16),
+		1);
 
-		graph_builder.AddPass(
-			RDG_EVENT_NAME("GaussianBlurCS"),
-			blur_params,
-			ERDGPassFlags::Compute,
-			[blur_cs, blur_params, group_count](FRHICommandList& rhi_cmd_list)
-			{
-				FComputeShaderUtils::Dispatch(rhi_cmd_list, blur_cs, *blur_params, group_count);
-			});
-	}
-
-	FRDGTextureRef noise_output = graph_builder.CreateTexture(desc, TEXT("gabor_noise_output"));
+	graph_builder.AddPass(
+		RDG_EVENT_NAME("GaussianBlurCS"),
+		blur_params,
+		ERDGPassFlags::Compute,
+		[blur_cs, blur_params, group_count](FRHICommandList& rhi_cmd_list)
+		{
+			FComputeShaderUtils::Dispatch(rhi_cmd_list, blur_cs, *blur_params, group_count);
+		});
+	
+	// Generate mips so that the amplitude estimator works
+	FGenerateMipsParams generate_mips_params = {};
+	FGenerateMips::Execute(graph_builder, GMaxRHIFeatureLevel, blur_output, generate_mips_params);
 
 	// gabor noise
+	FRDGTextureRef noise_output = graph_builder.CreateTexture(desc, TEXT("gabor_noise_output"));
+
 	TShaderMapRef<FGaborNoiseEnhancementCS> noise_cs(GetGlobalShaderMap(GMaxRHIFeatureLevel));
-	FGaborNoiseEnhancementCS::FParameters* noise_params =
-		graph_builder.AllocParameters<FGaborNoiseEnhancementCS::FParameters>();
+	FGaborNoiseEnhancementCS::FParameters* noise_params = graph_builder.AllocParameters<FGaborNoiseEnhancementCS::FParameters>();
 
 	noise_params->input_foveated = blur_output;								// <-- chain
 	noise_params->LinearSampler = TStaticSamplerState<SF_Bilinear>::GetRHI();
@@ -88,7 +89,7 @@ void FGaborEnhancementViewExtension::PrePostProcessPass_RenderThread(
 	noise_params->impulses_per_cell = impulses_per_cell;
 	noise_params->seed = seed;
 
-	const FIntVector group_count(
+	const FIntVector gabor_group_count(
 		FMath::DivideAndRoundUp(desc.Extent.X, 16),
 		FMath::DivideAndRoundUp(desc.Extent.Y, 16),
 		1);
@@ -97,9 +98,9 @@ void FGaborEnhancementViewExtension::PrePostProcessPass_RenderThread(
 		RDG_EVENT_NAME("GaborNoiseEnhancementCS"),
 		noise_params,
 		ERDGPassFlags::Compute,
-		[noise_cs, noise_params, group_count](FRHICommandList& rhi_cmd_list)
+		[noise_cs, noise_params, gabor_group_count](FRHICommandList& rhi_cmd_list)
 		{
-			FComputeShaderUtils::Dispatch(rhi_cmd_list, noise_cs, *noise_params, group_count);
+			FComputeShaderUtils::Dispatch(rhi_cmd_list, noise_cs, *noise_params, gabor_group_count);
 		});
 	
 
