eddd3f58ffbb565dac7c187861ae64d0c996a9d6
ville <lilinitsy@gmail.com>
2026-07-19 17:00:09 -0500

    Some bugfix for rare edgecases where noise cycles too much

--- Source/NoiseVelocity/Experiment2Manager.h
+++ Source/NoiseVelocity/Experiment2Manager.h
@@ -125,10 +125,10 @@ public:
 	float high_frequency_scale = 0.5f;
 
 	UPROPERTY(EditAnywhere, Category = "Foveation Levels")
-	float low_blur_rate_arcmin_per_degree = 0.25f;
+	float low_blur_rate_arcmin_per_degree = 0.17f;
 
 	UPROPERTY(EditAnywhere, Category = "Foveation Levels")
-	float high_blur_rate_arcmin_per_degree = 0.34f;
+	float high_blur_rate_arcmin_per_degree = 0.25f;
 
 	uint32 current_trial_index = 0;
 	float current_velocity_magnitude = 0.0f;
